68bf80521a38ccdeae5411e68b0016f41d16bed2
Khushboo Pandey <93470011+Khush141094@users.noreply.github.com>
2022-03-20 15:02:33 +0530

    Add files via upload

---
+++ Worksheet_set_1/Palindrome Program.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+string=input(("Enter a letter:"))  
+
+if(string==string[::-1]):  
+      print("The letter is a palindrome")  
+else:  
+      print("The letter is not a palindrome") 
+
+
+# In[ ]:
+
+
+
+
