bd7a9fab37016b7a62bd66921fc710063ff007f7
Khushboo Pandey <93470011+Khush141094@users.noreply.github.com>
2022-03-20 15:32:20 +0530

    Add files via upload

---
+++ Worksheet_set_1/Hypotenuse Program.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+
+a = float(input("Enter base: "))
+b = float(input("Enter height: "))
+
+c = math.sqrt(a ** 2 + b ** 2)
+
+print("Hypotenuse =", c)
+
+
+# In[ ]:
+
+
+
+
