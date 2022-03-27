24a24a4bc8cc388b7a53cde9f248e913cdad3f3b
Khushboo Pandey <93470011+Khush141094@users.noreply.github.com>
2022-03-27 23:05:15 +0530

    Add files via upload

---
+++ Web Scraping/Program 5(c)- Top 10 ODI bowlers.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from bs4 import BeautifulSoup
+import requests
+page=requests.get("https://www.icc-cricket.com/rankings/mens/player-rankings/odi/bowling")
+page
+
+page.content
+
+soup=BeautifulSoup(page.content,'html.parser')
+soup
+
+name=soup.find_all('div',class_="rankings-block__banner--name-large")
+name
+
+
+names=[] 
+for i in name:
+    names.append(i.get_text())
+names   
+
+
+name=soup.find_all('td',class_="table-body__cell rankings-table__name name")
+name[:9]
+
+
+for i in name:
+    names.append(i.get_text().replace('\n', ' '))
+names[:10]    
+
+
+team=soup.find_all('div',class_="rankings-block__banner--nationality")
+team
+
+
+teams=[] 
+for i in team:
+    teams.append(i.get_text().replace('\n', ''))
+teams    
+
+
+team=soup.find_all('span',class_="table-body__logo-text")
+team[:9]
+
+
+for i in team:
+    teams.append(i.get_text())
+teams[:10]    
+
+
+rating=soup.find_all('div',class_="rankings-block__banner--rating")
+rating
+
+
+ratings=[] 
+for i in rating:
+    ratings.append(i.get_text())
+ratings  
+
+
+rating=soup.find_all('td',class_="table-body__cell rating")
+rating[:9]
+
+
+for i in rating:
+    ratings.append(i.get_text())
+ratings[:10]   
+
+
+career_best=soup.find_all('span',class_="rankings-block__career-best-text")
+career_best
+
+career_best_rating=[]  
+for i in career_best:
+    career_best_rating.append(i.get_text().replace('\n', ' ').replace('  ',''))
+career_best_rating 
+
+
+career_best=soup.find_all('td',class_="table-body__cell u-text-right u-hide-phablet")
+career_best[:9]
+
+
+for i in career_best:
+    career_best_rating.append(i.get_text().replace('\n', ' ').replace('                        ',' '))
+career_best_rating[:10]   
+
+
+print(len(names),len(teams),len(ratings),len(career_best_rating))
+
+
+import pandas as pd
+top10=pd.DataFrame({})
+top10['Name']=names[:10]
+top10['Team']=teams[:10]
+top10['Rating']=ratings[:10]
+top10['Career_best_rating']=career_best_rating[:10]
+
+top10
+
+
+# In[ ]:
+
+
+
+
---
+++ Web Scraping/Program 6(a)-  Top 10 ODI Women's team.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+page=requests.get("https://www.icc-cricket.com/rankings/womens/team-rankings/odi")
+page
+
+page.content
+
+soup=BeautifulSoup(page.content,'html.parser')
+soup
+
+team=soup.find_all('span',class_="u-hide-phablet")
+team[:10]
+
+teams=[]
+for i in team:
+    teams.append(i.get_text())
+teams[:10]
+
+
+match=soup.find_all('td',class_="rankings-block__banner--matches")
+match
+
+
+Matches=[]  
+for i in match:
+    Matches.append(i.get_text().replace('\n',''))
+Matches
+
+
+match=soup.find_all('td',class_="table-body__cell u-center-text")
+match[:9]
+
+del match[1::2]
+
+
+for i in match:
+    Matches.append(i.get_text().replace('\n',''))
+Matches[:10]
+
+points=soup.find_all('td',class_="rankings-block__banner--points")
+points
+
+
+Points=[]  
+for i in points:
+    Points.append(i.get_text().replace('\n',''))
+Points
+
+points=soup.find_all('td',class_="table-body__cell u-center-text")
+points[:10]
+
+del points[::2]
+
+for i in points:
+    Points.append(i.get_text().replace('\n',''))
+Points[:10]
+
+rating=soup.find_all('td',class_="rankings-block__banner--rating u-text-right")
+rating
+
+
+Rating=[]  
+for i in rating:
+    Rating.append(i.get_text().replace('\n','').strip())
+Rating
+
+rating=soup.find_all('td',attrs={'class':'table-body__cell u-text-right rating'})
+rating[:9]
+
+for i in rating:
+    Rating.append(i.get_text().replace('\n',''))
+Rating
+
+Team_Rankings=pd.DataFrame({})
+Team_Rankings['Team']=teams[:10]
+Team_Rankings['Matches']=Matches[:10]
+Team_Rankings['Points']=Points[:10]
+Team_Rankings['Rating']=Rating[:10]
+
+
+Team_Rankings
+
+
+# In[ ]:
+
+
+
+
